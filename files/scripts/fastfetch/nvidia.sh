2266efc0230820644302ff11862a2318ae94a9ee
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-04-12 02:15:51 +0300

    Create nvidia.sh

---
+++ files/scripts/fastfetch/nvidia.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+TARGET_FILE="/usr/share/ublue-os/bazzite/fastfetch.jsonc"
+
+if [ -f "$TARGET_FILE" ]; then
+    sed -i -c 's/\("text": "\)usr\/libexec\/bazzite-fetch-image\("\)/\1shadowos-linux-nvidia:latest\2/' "$TARGET_FILE"
+else
+    echo "Error: $TARGET_FILE not found."
+    exit 1
+fi
+
