d2975bbb9f0dbeec35148047728b34193b9061c4
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-04-12 16:25:35 +0300

    Update fastfetch.sh

--- files/scripts/fastfetch.sh
+++ files/scripts/fastfetch.sh
@@ -4,4 +4,5 @@ set -oue pipefail
 TARGET_FILE="/usr/share/ublue-os/bazzite/fastfetch.jsonc"
 mkdir -p $(dirname "$TARGET_FILE")
 curl -Lo "$TARGET_FILE" https://raw.githubusercontent.com/ublue-os/bazzite/refs/heads/main/system_files/desktop/shared/usr/share/ublue-os/bazzite/fastfetch.jsonc
+sed -i 's|"type": "command"|"type": "os"|g' "$TARGET_FILE"
 sed -i 's|"text": "/usr/libexec/bazzite-fetch-image"|"format": "{variant-id}"|g' "$TARGET_FILE"
