7dd88453f8b30df6f645aca130a39e2b95a81af1
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-04-12 14:36:48 +0300

    Create normal.sh

---
+++ files/scripts/normal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -oue pipefail
+
+TARGET_FILE="/usr/share/ublue-os/bazzite/fastfetch.jsonc"
+mkdir -p $(dirname "$TARGET_FILE")
+curl -Lo "$TARGET_FILE" https://raw.githubusercontent.com/ublue-os/bazzite/refs/heads/main/system_files/desktop/shared/usr/share/ublue-os/bazzite/fastfetch.jsonc
+sed -i 's|"/usr/libexec/bazzite-fetch-image"|"shadowos-linux:latest"|g' "$TARGET_FILE"
