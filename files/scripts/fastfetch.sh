b23dd31748934b89b7894296c16394a08a01d888
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-04-14 19:49:06 +0300

    Update fastfetch.sh

--- files/scripts/fastfetch.sh
+++ files/scripts/fastfetch.sh
@@ -9,17 +9,5 @@ sed -i 's|"text": "/usr/libexec/bazzite-fetch-image"|"format": "{variant-id}"|g'
 sed -i 's|"1": "94"|"1": "31"|g' "$TARGET_FILE"
 sed -i '/"[2-6]": "[0-9]*"/d' "$TARGET_FILE"
 sed -i 's/31",/31"/g' "$TARGET_FILE"
-sed -i 's/"keys": "light_blue"/"keys": "red"/g' "$TARGET_FILE"
-sed -i 's/"user": "light_blue"/"user": "red"/g' "$TARGET_FILE"
-sed -i '/"modules": \[/a \
-        { "type": "title", "key": " 󱓚", "keyColor": "red" }, \
-        { "type": "command", "key": " 󰸗", "text": "date '\''+%A %d, %B %Y'\''" }, \
-        { "type": "command", "key": " 󱈔", "text": "curl -s https://orthocal.info/api/ > /tmp/ortho.json && jq -r .summary_title /tmp/ortho.json" }, \
-        { "type": "command", "key": " 󰦕", "text": "jq -r .feast_level_description /tmp/ortho.json" }, \
-        { "type": "command", "key": " 󱗗", "text": "jq -r .fast_level_desc /tmp/ortho.json" }, \
-        { "type": "command", "key": " 󱠗", "text": "jq -r .fast_exception_desc /tmp/ortho.json" }, \
-        { "type": "command", "key": " 󰚚", "text": "jq -r '\''.saints[0:2] | join(\", \")'\'' /tmp/ortho.json" }, \
-        { "type": "command", "key": " 󰗊", "text": "jq -r '\''.readings[0].display'\'' /tmp/ortho.json" }, \
-        "break",' "$TARGET_FILE"
 
 echo -e "\ncd ~\nclear\nfastfetch" >> "/etc/skel/.bashrc"
