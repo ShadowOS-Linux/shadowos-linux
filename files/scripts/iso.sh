19dda1c7aa3af922004cf19bb66c80630b39a61b
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-04-23 18:04:25 +0300

    Update iso.sh

--- files/scripts/iso.sh
+++ files/scripts/iso.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eoux pipefail
 
-dnf install -y anaconda-live libblockdev-btrfs firefox --setopt=disable_excludes=*
+dnf install -y anaconda-live firefox lvm2 device-mapper libblockdev-{btrfs,lvm,crypto,mdraid,loop} util-linux --setopt=disable_excludes=*
 sed -i '/\[Desktop Entry\]/a NoDisplay=true' /usr/share/applications/org.mozilla.firefox.desktop
