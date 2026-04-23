1b652781763f86c736cedbc73306408114ffd38e
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-04-23 17:44:03 +0300

    Update iso.sh

--- files/scripts/iso.sh
+++ files/scripts/iso.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -eoux pipefail
 
-dnf install -y anaconda-live libblockdev-btrfs --setopt=disable_excludes=*
+dnf install -y anaconda-live libblockdev-btrfs firefox --setopt=disable_excludes=*
+sed -i '/\[Desktop Entry\]/a NoDisplay=true' /usr/share/applications/org.mozilla.firefox.desktop
