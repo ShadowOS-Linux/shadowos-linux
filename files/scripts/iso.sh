7083a51e653428cfae84ced5a5a5af1b6c70747c
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-04-24 15:45:26 +0300

    Delete files/scripts/iso.sh

--- files/scripts/iso.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -eoux pipefail
-
-dnf install -y anaconda-live firefox lvm2 device-mapper libblockdev-{btrfs,lvm,crypto,mdraid,loop} util-linux --setopt=disable_excludes=*
-sed -i '/\[Desktop Entry\]/a NoDisplay=true' /usr/share/applications/org.mozilla.firefox.desktop
