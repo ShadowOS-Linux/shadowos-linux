de54c1abf851fa25a6d54e6a8d85f5b9c61e3e70
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-05-02 14:41:48 +0300

    Update bloat.sh

--- files/scripts/bloat.sh
+++ files/scripts/bloat.sh
@@ -9,6 +9,7 @@ APPS_TO_HIDE=(
     "waydroid-container-restart"
     "com.gerbilsoft.rom-properties.rp-config"
     "org.gnome.Yelp"
+    "scrcpy-console.desktop"
 )
 
 for app in "${APPS_TO_HIDE[@]}"; do
