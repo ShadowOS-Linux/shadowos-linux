49e43e08005092e7d0345594aa5f5c2bbb9c02eb
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-05-02 13:17:37 +0300

    Update fix.sh

--- files/system/etc/profile.d/fix.sh
+++ files/system/etc/profile.d/fix.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # sometimes /etc/skel doesn't work for the first user so this script should fix it and delete itself
 cp -rfp /etc/skel/. "$HOME/"
-rm -f /etc/profile.d/fix-home.sh
+rm -f /etc/profile.d/fix.sh
