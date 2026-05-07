32e3cbfc29f614ab896abe63d2cb451be48bc669
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-05-07 21:55:23 +0300

    Create uad.sh

---
+++ files/scripts/uad.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+
+LATEST_URL=$(curl -s https://api.github.com/repos/Universal-Debloater-Alliance/universal-android-debloater-next-generation/releases/latest \
+| grep "browser_download_url" \
+| grep -w "uad-ng-linux" \
+| cut -d '"' -f 4)
+
+TARGET="/usr/bin/uad-ng"
+
+if [ -n "$LATEST_URL" ]; then
+    echo "Downloading UAD-ng: $LATEST_URL"
+    curl -L "$LATEST_URL" -o "$TARGET"
+    
+    chmod +x "$TARGET"
+    echo "Installation complete."
+else
+    echo "Error: Could not isolate the specific Linux binary URL."
+    exit 1
+fi
