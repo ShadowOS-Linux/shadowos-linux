da5a4ab760f7d7ae9b18b21c3c41b402d0576a4d
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-05-07 22:26:41 +0300

    Update uad.sh

--- files/scripts/uad.sh
+++ files/scripts/uad.sh
@@ -4,12 +4,13 @@ set -euo pipefail
 LATEST_URL=$(curl -s https://api.github.com/repos/Universal-Debloater-Alliance/universal-android-debloater-next-generation/releases/latest \
 | grep "browser_download_url" \
 | grep -w "uad-ng-linux" \
+| head -n 1 \
 | cut -d '"' -f 4)
 
-TARGET="/usr/bin/uad-ng"
+TARGET="/usr/bin/uad"
 
 if [ -n "$LATEST_URL" ]; then
-    echo "Downloading UAD-ng: $LATEST_URL"
+    echo "Downloading UAD: $LATEST_URL"
     curl -L "$LATEST_URL" -o "$TARGET"
     
     chmod +x "$TARGET"
