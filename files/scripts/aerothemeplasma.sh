89f4e7adaa8564ae54031454b0b23fc9a329f88c
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-06-28 01:46:31 +0300

    Update aerothemeplasma.sh

--- files/scripts/aerothemeplasma.sh
+++ files/scripts/aerothemeplasma.sh
@@ -94,22 +94,28 @@ cd ..
 dnf install -y gcc gcc-c++ cmake make extra-cmake-modules plasma-workspace-devel libksysguard-devel unzip kvantum qt6-qtmultimedia-devel qt6-qt5compat-devel libplasma-devel qt6-qtbase-devel qt6-qtwayland-devel plasma-activities-devel kf6-kpackage-devel kf6-kglobalaccel-devel qt6-qtsvg-devel wayland-devel plasma-wayland-protocols kf6-ksvg-devel kf6-kcrash-devel kf6-kguiaddons-devel kf6-kcmutils-devel kf6-kio-devel kdecoration-devel kf6-ki18n-devel kf6-knotifications-devel kf6-kirigami-devel kf6-kiconthemes-devel cmake gmp-ecm-devel kf5-plasma-devel libepoxy-devel kwin-devel kf6-karchive kf6-karchive-devel plasma-wayland-protocols-devel qt6-qtbase-private-devel qt6-qtbase-devel kf6-knewstuff-devel kf6-knotifyconfig-devel kf6-attica-devel kf6-krunner-devel kf6-kdbusaddons-devel kf6-sonnet-devel plasma5support-devel plasma-activities-stats-devel polkit-qt6-1-devel qt-devel libdrm-devel kf6-kitemmodels-devel kf6-kstatusnotifieritem-devel kf6-frameworkintegration-devel wayland-protocols-devel --setopt=disable_excludes=*
 
 git clone https://gitgud.io/wackyideas/aerothemeplasma.git aerothemeplasma
+cd aerothemeplasma
 ## fake sudo for the theme install script
 function sudo() { "$@"; }
 export -f sudo
 
-LIBEXEC_DIR=libexec UAC_LIBEXEC_DIR=libexec/kf6 bash aerothemeplasma/install.sh --skip-x11
+LIBEXEC_DIR=libexec UAC_LIBEXEC_DIR=libexec/kf6 bash install.sh --skip-x11
 
 ## removing fake sudo
 unset -f sudo
 
+cd ..
+
 # execbin
 git clone https://gitgud.io/catpswin56/execbin
-bash execbin/install.sh
-bash execbin/add_rule.sh
+cd execbin
+bash install.sh
+bash add_rule.sh
+cd ..
 
 # gtk theme
 git clone https://gitgud.io/Gamer95875/Windows-7-Better /etc/skel/.themes/Windows-7-Better
+ln -s ../.themes/Windows-7-Better/gtk-4.0 /etc/skel/.config/gtk-4.0
 chmod +x /usr/libexec/topgrade/windows-7-gtk-theme-update
 echo '"Windows 7 GTK Theme" = "/usr/libexec/topgrade/windows-7-gtk-theme-update"' >> /etc/ublue-os/topgrade.toml
 
