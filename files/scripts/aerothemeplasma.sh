da757ac9543f2ff79e31ce06309ca32b277036d5
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-06-29 02:01:25 +0300

    Update aerothemeplasma.sh

--- files/scripts/aerothemeplasma.sh
+++ files/scripts/aerothemeplasma.sh
@@ -5,13 +5,13 @@ mkdir -p /tmp/win7theme/
 cd /tmp/win7theme/
 
 # fonts (script by github.com/winblues)
-git clone --depth=1 https://github.com/mrbvrz/segoe-ui-linux
+git clone --depth=1 --single-branch https://github.com/mrbvrz/segoe-ui-linux
 mkdir -p /usr/share/fonts/Microsoft/TrueType/SegoeUI/
 mv segoe-ui-linux/font/* /usr/share/fonts/Microsoft/TrueType/SegoeUI
 fc-cache -f /usr/share/fonts/Microsoft/TrueType/SegoeUI/
 
 # plymouth
-git clone --depth=1 https://github.com/furkrn/PlymouthVista
+git clone --depth=1 --single-branch https://github.com/furkrn/PlymouthVista
 cd PlymouthVista
 
 bash compile.sh
@@ -36,7 +36,7 @@ cd ..
 ## theme dependencies
 dnf install -y gcc gcc-c++ cmake make extra-cmake-modules plasma-workspace-devel libksysguard-devel unzip kvantum qt6-qtmultimedia-devel qt6-qt5compat-devel libplasma-devel qt6-qtbase-devel qt6-qtwayland-devel plasma-activities-devel kf6-kpackage-devel kf6-kglobalaccel-devel qt6-qtsvg-devel wayland-devel plasma-wayland-protocols kf6-ksvg-devel kf6-kcrash-devel kf6-kguiaddons-devel kf6-kcmutils-devel kf6-kio-devel kdecoration-devel kf6-ki18n-devel kf6-knotifications-devel kf6-kirigami-devel kf6-kiconthemes-devel cmake gmp-ecm-devel kf5-plasma-devel libepoxy-devel kwin-devel kf6-karchive kf6-karchive-devel plasma-wayland-protocols-devel qt6-qtbase-private-devel qt6-qtbase-devel kf6-knewstuff-devel kf6-knotifyconfig-devel kf6-attica-devel kf6-krunner-devel kf6-kdbusaddons-devel kf6-sonnet-devel plasma5support-devel plasma-activities-stats-devel polkit-qt6-1-devel qt-devel libdrm-devel kf6-kitemmodels-devel kf6-kstatusnotifieritem-devel kf6-frameworkintegration-devel wayland-protocols-devel kscreenlocker-devel --setopt=disable_excludes=*
 
-git clone --depth=1 https://gitgud.io/wackyideas/aerothemeplasma.git aerothemeplasma
+git clone --depth=1 --single-branch https://gitgud.io/wackyideas/aerothemeplasma.git aerothemeplasma
 cd aerothemeplasma
 
 ## temp fix
@@ -56,7 +56,7 @@ CMAKE_GENERATOR=Ninja LIBEXEC_DIR=libexec UAC_LIBEXEC_DIR=libexec/kf6 bash insta
 cd ..
 
 # execbin
-git clone --depth=1 https://gitgud.io/catpswin56/execbin
+git clone --depth=1 --single-branch https://gitgud.io/catpswin56/execbin
 cd execbin
 bash install.sh --ninja
 bash add_rule.sh
@@ -66,7 +66,7 @@ cd ..
 unset -f sudo
 
 # gtk theme
-git clone --depth=1 https://gitgud.io/Gamer95875/Windows-7-Better /etc/skel/.themes/Windows-7-Better
+git clone --depth=1 --single-branch https://gitgud.io/Gamer95875/Windows-7-Better /etc/skel/.themes/Windows-7-Better
 ln -s ../.themes/Windows-7-Better/gtk-4.0 /etc/skel/.config/gtk-4.0
 chmod +x /usr/libexec/topgrade/windows-7-gtk-theme-update
 echo '"Windows 7 GTK Theme" = "/usr/libexec/topgrade/windows-7-gtk-theme-update"' >> /etc/ublue-os/topgrade.toml
