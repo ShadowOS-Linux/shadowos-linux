0ceb857eda2f07ffa5a48ca03c3ff740fa1bf9e7
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-07-04 21:33:54 +0300

    Update steam.sh

--- files/scripts/steam.sh
+++ files/scripts/steam.sh
@@ -5,3 +5,7 @@ set -euo pipefail
 if [ -f "/usr/share/applications/bazzite-steam-bpm.desktop" ]; then
     rm /usr/share/applications/bazzite-steam-bpm.desktop
 fi
+
+if [ -d "/usr/share/gnome-shell/extensions/add-to-steam@pupper.space" ]; then
+    rm -rf /usr/share/gnome-shell/extensions/add-to-steam@pupper.space
+fi
