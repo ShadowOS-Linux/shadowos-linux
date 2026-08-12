b1ab83f2b55941e8446877f9a2b79fa4a3d315f4
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-08-12 12:50:28 +0300

    Update themes.sh

--- files/scripts/themes.sh
+++ files/scripts/themes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git clone --depth=1 --single-branch https://github.com/rafaelmardojai/firefox-gnome-theme.git /etc/mozilla/browser/gnome/
-git clone --depth=1 --single-branch https://github.com/MrOtherGuy/firefox-csshacks.git /etc/mozilla/browser/csshacks/
-git clone --depth=1 --single-branch https://github.com/rafaelmardojai/thunderbird-gnome-theme.git /etc/mozilla/email/gnome/
+git clone --depth=1 --single-branch https://github.com/rafaelmardojai/firefox-gnome-theme.git /etc/themes/mozilla/browser/gnome/
+git clone --depth=1 --single-branch https://github.com/MrOtherGuy/firefox-csshacks.git /etc/themes/mozilla/browser/csshacks/
+git clone --depth=1 --single-branch https://github.com/rafaelmardojai/thunderbird-gnome-theme.git /etc/themes/mozilla/email/gnome/
