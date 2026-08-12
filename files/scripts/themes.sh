40b975cfbbf320a2fd39e1e17ad98f1026519447
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-08-12 13:26:25 +0300

    Delete files/scripts/themes.sh

--- files/scripts/themes.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-git clone --depth=1 --single-branch https://github.com/rafaelmardojai/firefox-gnome-theme.git /etc/themes/mozilla/browser/gnome/
-git clone --depth=1 --single-branch https://github.com/MrOtherGuy/firefox-csshacks.git /etc/themes/mozilla/browser/csshacks/
-git clone --depth=1 --single-branch https://github.com/rafaelmardojai/thunderbird-gnome-theme.git /etc/themes/mozilla/email/gnome/
