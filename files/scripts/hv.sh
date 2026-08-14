3568479940f5aaf8269a607098f75ea2816e6c8f
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-08-14 14:09:45 +0300

    Create hv.sh

---
+++ files/scripts/hv.sh
@@ -0,0 +1,4 @@
+#!/usr/env/bash
+set -euo pipefail
+curl -L --fail "https://github.com/brcly/linuwux-runtime/releases/latest/download/liblinuwux.so" -o /usr/lib64/liblinuwux.so
+chmod 0755 /usr/lib64/liblinuwux.so
