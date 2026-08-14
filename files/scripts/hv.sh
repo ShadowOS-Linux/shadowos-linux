34b231887f471876df3c8c2b06905f1159d9ed5e
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-08-14 14:48:33 +0300

    Update hv.sh

--- files/scripts/hv.sh
+++ files/scripts/hv.sh
@@ -1,4 +1,4 @@
-#!/usr/env/bash
+#!/usr/bin/env bash
 set -euo pipefail
 curl -L --fail "https://github.com/brcly/linuwux-runtime/releases/latest/download/liblinuwux.so" -o /usr/lib64/liblinuwux.so
 chmod 0755 /usr/lib64/liblinuwux.so
